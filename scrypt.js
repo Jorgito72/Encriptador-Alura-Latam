9c33276996b39e4b7f00133b1f50d6d2d2202307
Jorge Pablo <pablojorgel01@gmail.com>
2023-04-28 16:24:44 -0300

    terminada la logica de las funciones

--- scrypt.js
+++ scrypt.js
@@ -7,7 +7,10 @@ const botonCopiar = document.getElementById("copiar");
 
 let divFinal=document.getElementById("div_final")
 let placeHolderFinal = document.getElementById("placehold_final");
+
+
 let textoFinal = document.getElementById("texto_final");
+let textoDesScrypt='';
 
 botonEncriptar.onclick = encriptar;
 botonDesencriptar.onclick = desencriptar;
@@ -16,26 +19,58 @@ botonCopiar.onclick = copiar;
 
 function encriptar() {
     let textoEntrada = document.getElementById("texto_entrada").value;
+    let textoEncriptado= textoEntrada.replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/a/gi, "ai").replace(/o/gi, "ober").replace(/u/gi, "ufat");
 
     placeHolderFinal.style.display = "none";
     textoFinal.style.display = "block";
     botonCopiar.style.display="inline";
-    textoFinal.value=textoEntrada +"Jorge";
 
-    console.log(textoEntrada);
+    textoFinal.value=textoEncriptado ;
+
+    // console.log(textoEntrada);
+    // console.log(textoEncriptado)
     // alert("Encriptando...");
 }
+
 function desencriptar() {
+    textoFinal.value='';
     let textoEntrada = document.getElementById("texto_entrada").value;
-    console.log(textoEntrada);
+   textoDesScrypt=textoEntrada.replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ai/gi, "a").replace(/ober/gi, "o").replace(/ufat/gi, "u");
+   textoFinal.value=textoDesScrypt;
+
+    console.log(textoDesScrypt );
+
+    
     // alert("Desecriptando...");
 }
 
+// La letra "e" es convertida para "enter"
+// La letra "i" es convertida para "imes"
+// La letra "a" es convertida para "ai"
+// La letra "o" es convertida para "ober"
+// La letra "u" es convertida para "ufat"
+
 function copiar() {
 
-    console.log(textoFinal);
+    console.log(textoFinal.value);
     let textoEntrada = document.querySelector("#texto_entrada");
     textoEntrada.value = textoFinal.value;
-    console.log(textoEntrada);
+    // console.log(textoEntrada);
     // alert("Copiando...");
 }
+ //function encriptar (){
+//     var texto = document.querySelector("#input-texto").value;
+//     var textoCifrado = texto.replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/a/gi, "ai").replace(/o/gi, "ober").replace(/u/gi, "ufat");
+//     document.querySelector(".text-input-salida").value = textoCifrado;
+//     document.querySelector("#input-texto").value;
+//     }
+
+
+    
+// var boton1 = document.querySelector("#btn-encriptar"); boton1.onclick = encriptar;
+
+// function desencriptar (){ var texto = document.querySelector("#input-texto").value; var textoCifrado = texto.replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ai/gi, "a").replace(/ober/gi, "o").replace(/ufat/gi, "u"); document.querySelector(".text-input-salida").value = textoCifrado; document.querySelector("#input-texto").value;
+
+// }
+
+// var boton2 = document.querySelector("#btn-desencriptar"); boton2.onclick = desencriptar;
